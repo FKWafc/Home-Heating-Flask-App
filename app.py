679c5f1bab6068b7e0a01dc03004a94f26119721
FKWafc <finn.kearns@gmail.com>
2021-12-18 01:56:33 +0000

    Added scraping tool in order to scrape data from other servers/webpages which contain heating information. Now I need to work on parsing the data in order for it to be used. right now its basically just a raw json file.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
 from flask_mqtt import Mqtt
+import urllib.request
+from urllib.parse import urlparse ,urljoin
+from bs4 import BeautifulSoup
+import scrapy
+import requests
 
 # Manually enter the mosquitto DB location and user info
 app = Flask(__name__)
@@ -23,10 +28,22 @@ def home():
         return render_template('home.html')
 
     if request.method == 'GET':
-        subscribe = request.form['subscribe']
-        data = mqtt.subscribe(subscribe)
+        #subscribe = request.form['subscribe']
+        #data = mqtt.subscribe(subscribe)
 
-        return '''<h1>{}</h1>'''.format(data)
+        #return '''<h1>{}</h1>'''.format(data)
+        return render_template('home.html')
+
+
+# adding scraping tool in order to scrape data from other servers/webpages which contain heating information
+# need to work on parsing the data in order for it to be used. right now its basically just a raw json file.
+@app.route('/solar')
+def solar():
+
+    st = requests.get('http://192.168.101.208/power.zhtml')
+    print(str(st) + 'hello')
+
+    return render_template('solar.html', string=st.text)
 
 # App runs in debug to allow for changes to occur without having to restart the Application
 
